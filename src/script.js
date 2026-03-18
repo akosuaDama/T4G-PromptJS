f2d9dbdab059db73f7981a41655beb95eecab912
ROSELYNADJETEY <roselynmingle386@gamil.com>
2026-03-18 07:25:51 -0700

    commenting out prompting user for email

--- src/script.js
+++ src/script.js
@@ -5,3 +5,5 @@ alert("Hello, " + name + "!");
 let age = prompt("How old are you?");
 alert("You are " + age + " years old. That's great!");
 
+
+// alert(`Hi, ${name}.you are ${age} years old and we will keep in touch via your email: ${email} 👋 👋`);
