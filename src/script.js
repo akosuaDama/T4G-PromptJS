69d7a9d3563a89f79b741cf8de02474a3e477729
ROSELYNADJETEY <roselynmingle386@gamil.com>
2026-03-18 07:30:29 -0700

    created the final part

--- src/script.js
+++ src/script.js
@@ -7,7 +7,9 @@ alert("You are " + age + " years old. That's great!")
 
 
 
-// alert(`Hi, ${name}.you are ${age} years old and we will keep in touch via your email: ${email} 👋 👋`);
+
 let email = prompt("What is your email address?");
 alert("Your email address is " + email + ". Thank you for sharing!");
+
+alert(`Hi, ${name}.you are ${age} years old and we will keep in touch via your email: ${email} 👋 👋`);
 
