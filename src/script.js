61a6a46da456d3eac532ea6d79b9cb02ee122146
iwezeneneh55-ux <iwezeneneh55@gmail.com>
2026-03-18 14:06:32 +0000

    adding changes

--- src/script.js
+++ src/script.js
@@ -3,5 +3,7 @@ alert("Hello, " + name + "!");
 
 
 let age = prompt("How old are you?");
-alert("You are " + age + " years old. That's great!");
+alert("You are " + age + " years old. That's great!")
 
+let email = prompt("What is your email address?");
+alert("Your email address is " + email + ". Thank you for sharing!");
