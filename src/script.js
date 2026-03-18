2d0e01817a23bde36bb51556c644b9376c27bc08
akosuaDama <damarisappiahdanso@gmail.com>
2026-03-18 14:46:16 +0000

    adding new changes

--- src/script.js
+++ src/script.js
@@ -1,12 +1,8 @@
 let name = prompt("What is your name?");
 alert("Hello, " + name + "!");
 
-
 let age = prompt("How old are you?");
 alert("You are " + age + " years old. That's great!")
-
-
-
 
 let email = prompt("What is your email address?");
 alert("Your email address is " + email + ". Thank you for sharing!");
